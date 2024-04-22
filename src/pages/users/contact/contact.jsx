05971dc2894bfdaa0ff01e42cb9440a04186f81f
thanhdung <ltdung0802@gmail.com>
2024-04-22 11:05:09 +0700

    add gg map

--- src/pages/users/contact/contact.jsx
+++ src/pages/users/contact/contact.jsx
@@ -35,8 +35,17 @@ function Contact() {
                         <button className="btn-send-lien-he">Gửi liên hệ</button></div>    
 
                     </div>
-                <div className="div-map " style={{marginBottom:100}}>
-                    c
+                <div className="div-map">
+                    <p>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!
+                            1d3918.746311985123!2d106.68519917590623!3d10.83071595818821!2m3!1f0!2f0!3f0!3m2!
+                            1i1024!2i768!4f13.1!3m3!1m2!1s0x317528f82c8b5185%3A0xd831de75ccfe2560!2zMjk4IMS
+                            QLiBRdeG6o25nIEjDoG0sIFBoxrDhu51uZyA1LCBHw7IgVuG6pXAsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!
+                            5e0!3m2!1svi!2s!4v1713757526626!5m2!1svi!2s"
+                            style={{width:"100%", height: "580px", border: "none"}}
+                            id="" title="map">
+                        </iframe>
+                    </p>
                 </div>
             </div>
        </div>
