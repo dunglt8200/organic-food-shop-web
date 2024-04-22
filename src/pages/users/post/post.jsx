a59ce78db86ed525fc723f927bf4c074ab8e813f
thanhdung <ltdung0802@gmail.com>
2024-04-22 17:37:54 +0700

    add  pagination

--- src/pages/users/post/post.jsx
+++ src/pages/users/post/post.jsx
@@ -3,10 +3,12 @@ import "./style_post.css";
 import { Posts } from "../../../utils/const";
 import PostItem from "../../common/post";
 import { shortenText } from "../../../utils/util";
+import { PaginatedList } from "react-paginated-list";
 
 function Post() {
     return (
         <div className="post-main">
+            {/* Tin nổi bậc */}
             <div className="div-hot-post">
                 <div className="div-title-hot-post">
                     <span>Tin nổi bật</span>
@@ -22,10 +24,19 @@ function Post() {
                     <span style={{fontWeight: "bold", fontSize: "xx-large"}}>Tin tức</span>
                     <span>(Có tất cả {Posts.length} bài viết)</span>
                 </div>
+                {/* Phân trang list*/}
                 <div className="div-post-main-list">
-                    {Posts.map((post_item, post_item_key)=> (
-                        <PostItem url={post_item.url} title={post_item.title.toUpperCase()} content={shortenText(post_item.content, 150)}></PostItem>
-                    ))}
+                    <PaginatedList list={Posts} itemsPerPage={4}
+                        renderList={(list) => (
+                            <>
+                              {list.map((post_item, post_item_key) => {
+                                return (
+                                    <PostItem url={post_item.url} title={post_item.title.toUpperCase()} content={shortenText(post_item.content, 150)}></PostItem>
+                                );
+                              })}
+                            </>
+                          )}
+                    />
                 </div>
             </div>
         </div>
