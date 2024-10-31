b7d0929e773a0101442c3b9ef51a912392090fdf
thanhdung <ltdung0802@gmail.com>
2024-10-31 16:34:45 +0700

    improve UI

--- src/pages/users/post/post.jsx
+++ src/pages/users/post/post.jsx
@@ -1,12 +1,22 @@
-import React from "react";
 import "./style_post.css";
 import { Posts } from "../../../utils/const";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { Link } from 'react-router-dom';
 import { PaginatedList } from "react-paginated-list";
 import { shortenText } from "../../../utils/util";
+import { React, useState } from "react";
 
 function Post() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 4;
+
+    const totalPages = Math.ceil(Posts.length / itemsPerPage);
+    const currentPosts = Posts.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
     return (
         <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center" }}>
              <div className="post-main">
@@ -29,9 +39,9 @@ function Post() {
                 </div>
                 {/* Phân trang list*/}
                 <div className="div-post-main-list">
-                    {Posts.map((item) => (
+                    {currentPosts.map((item) => (
                         <div className="grid-post-item">
-                            <img style={{width: "90%", height: 200, borderRadius: 20 }} src={item.url}></img>
+                            <img style={{ width: 400, height: 200, borderRadius: 20 }} src={item.url}></img>
                             <span style={{ color: "black", fontWeight: "bold", fontSize: 15 }}>{item.title.toUpperCase()}</span> 
                             <span style={{ color: "#656565" }}>{shortenText(item.content, 100, false)}</span>
                             <div className="div-read-more">
@@ -42,6 +52,25 @@ function Post() {
                             </div>                                                          
                         </div>                            
                     ))}                                  
+                </div>
+                <div className="pagination-controls">
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <button
+                            key={index + 1}
+                            onClick={() => handlePageChange(index + 1)}
+                            style={{
+                                margin: "10px 5px",
+                                padding: "5px 10px",
+                                backgroundColor: currentPage === index + 1 ? "#fe9614" : "#f0f0f0",
+                                color: currentPage === index + 1 ? "white" : "black",
+                                border: "none",
+                                borderRadius: "4px",
+                                cursor: "pointer",
+                            }}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
                 </div>
             </div>
         </div>
