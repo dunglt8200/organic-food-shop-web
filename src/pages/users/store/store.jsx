ac840ec590526ba06ea014d566e1549bf135a4e3
thanhdung <ltdung0802@gmail.com>
2024-12-02 17:23:13 +0700

    improve page store

--- src/pages/users/store/store.jsx
+++ src/pages/users/store/store.jsx
@@ -1,22 +1,26 @@
 import { React, useState } from "react";
 import { Categorys, SanPhams } from "../../../utils/const";
 import "./style_store.css";
-import CategoryProduct from "../../common/categoryProduct";
 import Product from "../../common/product";
 
 function Store() {
     const [activeIndex, setActiveIndex] = useState(1);
     const handleMenuClick = (index) => {
         setActiveIndex(index);
-        console.log(index)
+        setCurrentPage(1);
     }
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 8;
+    let sanPhamFilters = SanPhams.filter(item => item.id_title === activeIndex);
+    const totalPages = Math.ceil(sanPhamFilters.length / itemsPerPage);
+    const currentSanPhams = sanPhamFilters.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
     return (
        <div className="main-store">
-            {/* { Categorys.map((item_category, key_category) => (
-                <CategoryProduct list_sanpham={SanPhams} id_category={item_category.id} name={item_category.name}></CategoryProduct>
-            ))} */}
-
             <div className="category-menu">
                 <div style={{ display: "flex", gap: 5, flexDirection: "column" }}>
                     <span style={{ fontSize: 20, fontWeight: "bold" }}>Danh mục sản phẩm</span>
@@ -24,15 +28,29 @@ function Store() {
                 </div>                
                 <ul className="ul-menu-category">
                     {Categorys.map((item_category) => (
-                        <li className="li-menu-category" onClick={() => handleMenuClick(item_category.id)}>{item_category.name}</li>
+                        <li className={`li-menu-category ${item_category.id === activeIndex ? 'active':''}`}  onClick={() => handleMenuClick(item_category.id)}>{item_category.name}</li>
                     ))}
                 </ul>            
             </div>
             <div className="main-product">
-                {SanPhams.filter(item => item.id_title == activeIndex).map((sp_item) => (
-                    <Product url={sp_item.url} name={sp_item.name} price={sp_item.price}></Product>
-                ))}                                    
-            </div>
+                <div className="div-list-product">
+                    {currentSanPhams.filter(item => item.id_title === activeIndex).map((sp_item) => (
+                        <Product url={sp_item.url} name={sp_item.name} price={sp_item.price}></Product>
+                    ))}                                  
+                </div>
+                <div className="pagination-controls">
+                {Array.from({ length: totalPages }, (_, index) => (
+                    <button
+                        key={index + 1}
+                        onClick={() => handlePageChange(index + 1)}
+                        style={{ margin: "10px 5px", padding: "5px 10px", backgroundColor: currentPage === index + 1 ? "#fe9614" : "#f0f0f0",
+                            color: currentPage === index + 1 ? "white" : "black", border: "none", borderRadius: "4px", cursor: "pointer",
+                            }}>
+                        {index + 1}
+                    </button>
+                    ))}
+                </div>              
+            </div>                 
        </div>
     )
 }
