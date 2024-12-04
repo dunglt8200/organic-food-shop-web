f31ad5ffda428d45e188697c6a6a5c9f72c240b5
thanhdung <ltdung0802@gmail.com>
2024-12-04 14:48:34 +0700

    improve page post

--- src/pages/common/hotPost.jsx
+++ src/pages/common/hotPost.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Posts } from "../../utils/const";
 import { Link } from "react-router-dom";
 import { ROUTERS } from "../../utils/router";
+import moment from "moment";
+import { CiClock2 } from "react-icons/ci";
 
 function HotPost() {
     return (
@@ -12,7 +14,12 @@ function HotPost() {
                         <Link style={{ color: "black", textDecoration: "none"}} to={`/${ROUTERS.USER.BAIVIET}/${post_item.id}`}>
                             <div key={post_item_key} className="div-hot-post-item">
                             <img style={{ width: "25%", height: "20%", borderRadius: 5 }} src={post_item.url} alt="" />
-                            <span style={{ fontSize: 17, color: "#231f20" }}>{post_item.title}</span>
+                            <div>
+                                <span style={{ fontSize: 17, color: "#231f20" }}>{post_item.title}</span>
+                                <span style={{display: "flex", alignContent: "center", gap: 5, color: "#808080"}}>
+                                    <CiClock2 />{moment().format("DD/MM/YYYY")}
+                                </span>
+                            </div>                            
                         </div>
                         </Link>               
                     ))}
